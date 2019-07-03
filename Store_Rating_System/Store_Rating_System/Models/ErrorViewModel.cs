065841d95e0ffa5edec7bbb5e9ae1ec087b422b9
Ivan Agafonov <sudden1306@gmail.com>
2019-07-03 06:26:10 +0000

    inital test project

---
+++ Store_Rating_System/Store_Rating_System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store_Rating_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
