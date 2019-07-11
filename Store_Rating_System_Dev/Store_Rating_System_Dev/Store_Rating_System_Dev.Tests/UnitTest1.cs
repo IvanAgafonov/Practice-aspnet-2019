aca805d01c38a26c0c7463e77beceeb698346e0f
Ivan Agafonov <sudden1306@gmail.com>
2019-07-11 19:00:06 +0000

    Inital dev project

---
+++ Store_Rating_System_Dev/Store_Rating_System_Dev/Store_Rating_System_Dev.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Store_Rating_System_Dev.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Store_Rating_System_Dev/Store_Rating_System_Dev/Store_Rating_System_Dev/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Store_Rating_System_Dev.Models;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Store_Rating_System_Dev.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(IFormFile uploadedFile)
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
